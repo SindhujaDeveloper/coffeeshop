3b3ad0d6c8d8f7c31aa0f6f94c4b2fc511180dab
SindhujaDeveloper <crazydeveloper.p@gmail.com>
2023-08-21 18:00:45 +0530

    menu list updated

--- src/utils/helpers/container/menu.tsx
+++ src/utils/helpers/container/menu.tsx
@@ -1,11 +1,14 @@
 import blackCoffee from "../../../assets/images/blackcoffee.png";
 import expresso from "../../../assets/images/expressoCoffee.png";
 import cappuccino from "../../../assets/images/cappuccino.png";
+import flatWhite from "../../../assets/images/flatWhiteCoffee.png";
+import artCoffee from "../../../assets/images/artCoffee.png";
+import mochaCoffee from "../../../assets/images/mochaCoffee.png";
 
 export const menuItems = [
   {
     name: "Black Coffee",
-    time: "10mins away",
+    time: "5mins away",
     rate: "RS.20",
     image: blackCoffee,
     id: 1,
@@ -13,41 +16,41 @@ export const menuItems = [
   },
   {
     name: "Expresso",
-    time: "2mins away",
-    rate: "RS.50",
+    time: "10mins away",
+    rate: "RS.10",
     image: expresso,
     id: 2,
     count: 0,
   },
   {
-    name: "Black Coffee",
-    time: "10mins away",
-    rate: "RS.20",
+    name: "Cappuccino",
+    time: "23mins away",
+    rate: "RS.90",
     image: cappuccino,
     id: 3,
     count: 0,
   },
   {
-    name: "Black Coffee",
-    time: "10mins away",
-    rate: "RS.20",
-    image: expresso,
+    name: "Flat White Coffee",
+    time: "20mins away",
+    rate: "RS.75",
+    image: flatWhite,
     id: 4,
     count: 0,
   },
   {
-    name: "Black Coffee",
-    time: "10mins away",
-    rate: "RS.20",
-    image: cappuccino,
+    name: "Art Coffee",
+    time: "30mins away",
+    rate: "RS.200",
+    image: artCoffee,
     id: 5,
     count: 0,
   },
   {
-    name: "Black Coffee",
-    time: "10mins away",
-    rate: "RS.20",
-    image: blackCoffee,
+    name: "Mocha Coffee",
+    time: "15mins away",
+    rate: "RS.150",
+    image: mochaCoffee,
     id: 6,
     count: 0,
   },
